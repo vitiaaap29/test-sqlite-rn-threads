9405cc5b424c9f295c72f482fa3889def1150ceb
Viktor Punko <viktor.punko@effective-soft.com>
2020-06-01 03:54:07 +0300

    sqlite

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
+import SQLite from 'react-native-sqlite-storage'
+
 export default function App() {
+  const db = SQLite.openDatabase({
+    name: 'TestDB.db',
+    location: 'default',
+    createFromLocation: '~www/test-v.db',
+  }, (s) => {
+    console.log('ddd', s)
+  }, (er) => {
+    console.warn('ddd ----',er)
+  })
+
+
+  function onClick() {
+    console.log('addd')
+    db.transaction((tx) => {
+      tx.executeSql('INSERT INTO Employees (name, desc) VALUES ("Sylvester Stallone", "sfdsfdsfa");')
+    })
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <TouchableOpacity onPress={onClick}>
+
       <Text style={styles.instructions}>To get started, edit App.js</Text>
+
+      </TouchableOpacity>
       <Text style={styles.instructions}>{instructions}</Text>
     </View>
   );
